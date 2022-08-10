fc30da4922fe58b95586670adc955af7909362c4
LokiTheDark666 <khcr.loki@mail.ru>
2022-08-10 20:06:24 +0300

    add readme

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,24 +1,28 @@
 package ru.netology;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
 
 import static com.codeborne.selenide.Condition.*;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class AuthTest {
+
     @Test
     void shouldRegistatedActive() {
+        Configuration.holdBrowserOpen = true;
         Registration registration = DataGenerator.getNewUser("active");
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=login] input").setValue(registration.getLogin());
         form.$("[data-test-id=password] input").setValue(registration.getPassword());
         form.$(".button").click();
-        $$(".heading").find(exactText("Личный кабинет")).shouldBe(exist);
+        $(withText("Личный кабинет")).shouldBe(exist);
     }
 
     @Test
